2e7eb2db77351c4bb867e6fdd75aad99e4e66444
phonghdpxd-cmd <phonghdpxd@gmail.com>
2026-08-21 05:01:55 +0630

    feat(construction): register TEAM 1 & TEAM 2 CONSTRUCTION groups in system config and auditor

--- tni_config.py
+++ tni_config.py
@@ -10,6 +10,14 @@ TELEGRAM_GROUPS = {
     "CONTROL": -5251698940, # 5 TNI TECHNICA DEP CONTROL SITE
 }
 
+# Telegram Construction Groups (T CONS)
+CONSTRUCTION_GROUPS = {
+    "T1_CONS": -5405599980,  # TEAM 1 CONSTRUCTION
+    "T2_CONS": -5006032995,  # TEAM 2 CONSTRUCTION
+    "T3_CONS": -5342629411,  # Team 3 CONSTRUCTION
+    "T4_CONS": -5473673421,  # TEAM 4 CONSTRUCTION
+}
+
 # Team Names (integer key mapping)
 TEAM_NAMES = {
     1: "Team 1 Dawei",
@@ -24,4 +32,8 @@ GROUP_NAMES = {
     "T2": "Team2 Myeik",
     "T3": "Team3 Bokpyin",
     "T4": "Team4 Kawthoung",
+    "T1_CONS": "Team 1 Construction",
+    "T2_CONS": "Team 2 Construction",
+    "T3_CONS": "Team 3 Construction",
+    "T4_CONS": "Team 4 Construction",
 }
